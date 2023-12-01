417ec66a46487a2a7b82569bde328c102b30aef0
IronC <hannirock123@gmail.com>
2023-12-01 13:44:32 +0800

    test deploy contract

---
+++ deploy-contract/index.js
@@ -0,0 +1,55 @@
+#! /opt/homebrew/bin/node
+const fs = require(`fs`)
+const {ethers} = require('ethers')
+
+require('dotenv').config();
+const l2PrivateKey = process.env.L2_PRIVATE_KEY;
+
+const optimismMintableERC20Factory = () => {
+    const provider = new ethers.JsonRpcProvider(process.env.L2_API);
+    const wallet = new ethers.Wallet(l2PrivateKey, provider)
+    const optimismMintableERC20FactoryAddress = process.env.L2_OPTIMISM_MINTABLE_ERC20_FACTORY_ADDRESS;
+    try {
+        const ftext = fs.readFileSync("node_modules/@eth-optimism/contracts-bedrock/forge-artifacts/OptimismMintableERC20Factory.sol/OptimismMintableERC20Factory.json").toString().replace(/\n/g, "")
+        const optimismMintableERC20FactoryData = JSON.parse(ftext);
+        try {
+            return new ethers.Contract(
+                optimismMintableERC20FactoryAddress,
+                optimismMintableERC20FactoryData.abi,
+                wallet
+            )
+        } catch (err) {
+            console.log(err)
+        }
+    } catch (err) {
+        console.log(err);
+    }
+}
+
+const deploy = async () => {
+    const factoryContract = optimismMintableERC20Factory();
+    // console.log(factoryContract.interface.fragments)
+    try {
+        const tx = await factoryContract.createOptimismMintableERC20(
+            process.env.L1_TOKEN_ADDRESS,
+            process.env.TOKEN_NAEME,
+            process.env.TOKEN_SYMBOL
+        );
+        console.log(tx);
+        const rcpt = await tx.wait();
+        console.log(rcpt)
+    } catch (err) {
+        console.log(err)
+    }
+}
+
+const main = async () => {
+    await deploy()
+}  // main
+
+main().then(() => process.exit(0))
+    .catch((error) => {
+        console.error(error)
+        process.exit(1)
+    })
+
--- standard-bridge-standard-token/hardhat.config.js
+++ standard-bridge-standard-token/hardhat.config.js
@@ -5,24 +5,31 @@ require('@nomiclabs/hardhat-ethers')
 require('dotenv').config();
 
 
-const words = process.env.MNEMONIC.match(/[a-zA-Z]+/g).length
-validLength = [12, 15, 18, 24]
-if (!validLength.includes(words)) {
-   console.log(`The mnemonic (${process.env.MNEMONIC}) is the wrong number of words`)
-   process.exit(-1)
-}
+// const words = process.env.MNEMONIC.match(/[a-zA-Z]+/g).length
+// validLength = [12, 15, 18, 24]
+// if (!validLength.includes(words)) {
+//    console.log(`The mnemonic (${process.env.MNEMONIC}) is the wrong number of words`)
+//    process.exit(-1)
+// }
 
 module.exports = {
   networks: {
-    'optimism-goerli': {
-      chainId: 420,
-      url: `https://opt-goerli.g.alchemy.com/v2/${process.env.L2_ALCHEMY_KEY}`,
-      accounts: { mnemonic: process.env.MNEMONIC }
-    },
-    'optimism-mainnet': {
-      chainId: 10,
-      url: `https://opt-mainnet.g.alchemy.com/v2/${process.env.L2_ALCHEMY_KEY}`,
-      accounts: { mnemonic: process.env.MNEMONIC }
+    // 'optimism-goerli': {
+    //   chainId: 420,
+    //   url: `https://opt-goerli.g.alchemy.com/v2/${process.env.L2_ALCHEMY_KEY}`,
+    //   accounts: { mnemonic: process.env.MNEMONIC }
+    // },
+    // 'optimism-mainnet': {
+    //   chainId: 10,
+    //   url: `https://opt-mainnet.g.alchemy.com/v2/${process.env.L2_ALCHEMY_KEY}`,
+    //   accounts: { mnemonic: process.env.MNEMONIC }
+    // },
+    'ipollo-sepolia': {
+      chainId: 1852,
+      url: `${process.env.L2URL}`,
+      accounts: {
+        mnemonic: `${process.env.MNEMONIC}`
+      }
     }
   },
   solidity: '0.8.13',
